49bf59e26844424772681c528523e955d8e3ba63
DzeBakuEso <dzeblebaku@gmail.com>
2024-11-26 14:30:38 -0800

    1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -7,22 +7,22 @@
 */
 
 if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+  process.stderr.write("Missing argument\n");
+  process.stderr.write("Usage: ./1-print_square.js <size>\n");
+  process.stderr.write("Example: ./1-print_square.js 8\n");
+  process.exit(1);
 }
 
-size = parseInt(process.argv[2]) // Remove the second argument for base 16
+size = parseInt(process.argv[2]); // Remove the second argument for base 16
 
 if (isNaN(size) || size <= 0) {
-    process.stderr.write("Size must be a positive integer\n");
-    process.exit(1);
+  process.stderr.write("Size must be a positive integer\n");
+  process.exit(1);
 }
 
 for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+  for (let j = 0; j < size; j++) {
+    process.stdout.write("#");
+  }
+  process.stdout.write("\n");
 }
