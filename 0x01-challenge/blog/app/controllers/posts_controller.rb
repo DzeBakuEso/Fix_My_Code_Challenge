112878f9dea8590ec7346b5566a52510ddfe230e
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-02 16:20:40 -0800

    controllers/posts_controller.rb

--- 0x01-challenge/blog/app/controllers/posts_controller.rb
+++ 0x01-challenge/blog/app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  
+
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.where(online: true).order('created_at DESC')  # Only show online posts
   end
 
   def new
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    if @post.update(params[:post].permit(:title, :body))
+    if @post.update(post_params)
       redirect_to @post
     else
       render 'edit'
@@ -44,8 +44,16 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  # New action to toggle the 'online' status of a post
+  def toggle_online
+    @post = Post.find(params[:id])
+    @post.update(online: !@post.online)  # Toggle the boolean value of 'online'
+    redirect_to @post
+  end
+
   private
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :online)  # Allow the 'online' field to be updated
+  end
 end
